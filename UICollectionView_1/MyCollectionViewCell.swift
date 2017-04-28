0831129678217227a741cd1c5f87b7ca57de08ef
Tran Van Tin <nit@Trans-Mac.local>
2017-04-28 22:32:46 +0700

    initial commit

---
+++ UICollectionView_1/MyCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  MyCollectionViewCell.swift
+//  UICollectionView_1
+//
+//  Created by Tran Van Tin on 4/28/17.
+//  Copyright © 2017 Tran Van Tin. All rights reserved.
+//
+
+import UIKit
+
+class MyCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet var myImageView: UIImageView!
+}
--- UICollectionView_1/ViewController.swift
+++ UICollectionView_1/ViewController.swift
@@ -8,11 +8,16 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+    
+    @IBOutlet var MyCollectionView: UICollectionView!
+    var images = ["one", "two", "three", "four", "five", "two", "three", "four", "five"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.MyCollectionView.delegate = self
+        self.MyCollectionView.dataSource = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +25,20 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return images.count
+    }
 
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "collection_cell", for: indexPath) as! MyCollectionViewCell
+            
+            //set images
+            cell.myImageView.image = UIImage(named: images[indexPath.row])
+            return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print("Selected item is ", indexPath.row)
+    }
 }
 
