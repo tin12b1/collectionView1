03d152d5f670b37dd9ee03297c4f4b8974abae9e
Tran Van Tin <nit@Trans-Mac.local>
2017-04-28 21:08:41 +0700

    Initial Commit

---
+++ UICollectionView_1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UICollectionView_1
+//
+//  Created by Tran Van Tin on 4/28/17.
+//  Copyright © 2017 Tran Van Tin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
